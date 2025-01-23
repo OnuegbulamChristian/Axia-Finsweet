4bff9f7150483dc41ba5450e6a753329233b5192
OnuegbulamChristian <christianonuegbulam@gmail.com>
2025-01-23 01:11:17 -0800

    hero section

--- src/Components/Herosection/Hero.tsx
+++ src/Components/Herosection/Hero.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { CiClock2 } from "react-icons/ci";
 import HeroImage from "../../assets/Header-Image.png";
-
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="h-max md:h-[160vh] w-full">
       <div className="h-max md:h-[150vh] w-[95%] mx-auto flex justify-center items-start gap-[30px] md:py-[5vh]">
-        <div className="w-full sm:w-[50%] h-max md:h-[150vh] flex flex-col justify-end items-start px-[30px] pt-[50px] md:pt-[0px] relative hero-content-div bg-yellow-500">
+        <div className="w-full sm:w-[50%] h-max md:h-[150vh] flex flex-col justify-end items-start px-[30px] pt-[50px] md:pt-[0px] relative hero-content-div">
           <h1 className="text-4xl w-[80%] sm:w-[100%] md:text-[60px] lg:text-[90px] leading-[45px] md:leading-[90px] lg:leading-[120px] font-medium text-white sm:text-black">
             Get your vehicle service done online at one place
           </h1>
